288bbb5dce0d77f5a6f9f43e99d556c7768abc1d
S-J-Kim <hedro0458@gmail.com>
2022-06-03 13:26:23 +0900

    fix errors

--- implementation/signup.h
+++ implementation/signup.h
@@ -26,15 +26,15 @@ private:
 	UserRepository *userlist;
 
 	// 파일 입출력 객체 포인터 선언
-	ifstream *fin;
-	ofstream *fout;
+	FILE *fin;
+	FILE *fout;
 
 public:
 	// main 함수의 진입점 함수에서 파일 스트림 객체를 매개변수로 받아서 멤버로 초기화
-	Signup(UserRepository *, ifstream *, ofstream *);
+	Signup(UserRepository *, FILE *, FILE *);
 	User *createNewUser();
 
 	// 파일스트림 객체의 getter
-	ifstream *getFin() { return this->fin; }
-	ofstream *getFout() { return this->fout; }
+	FILE *getFin() { return this->fin; }
+	FILE *getFout() { return this->fout; }
 };
